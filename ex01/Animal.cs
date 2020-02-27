c52c45c7d0a951880d6874a7c98ee50ea86d26ed
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-27 21:30:16 +0200

    Add project files.

---
+++ ex01/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    class Animal
+    {
+        public string nume;
+        public int age;
+        public float happiness;
+
+        public void baseAnimal()
+        {
+            Console.WriteLine("Name:" + nume);
+            Console.WriteLine("Age:" + age);
+            Console.WriteLine("Happiness: " + happiness);
+        }
+
+    }
+
+    class Dog : Animal
+    {
+        public int spotCount;
+        public void Bark()
+        {
+            Console.WriteLine("WUF");
+        }
+    }
+    class Cat : Animal 
+    {
+        public float cuteness;
+        public void Meow()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+}
---
+++ ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog spotty = new Dog();
+            spotty.nume = "Beethoven";
+            spotty.age = 4;
+            spotty.happiness = 0.8f;
+            spotty.spotCount = 25;
+            spotty.baseAnimal();
+            spotty.Bark();
+
+            Cat heisenberg = new Cat();
+            heisenberg.nume = "Heisenberg";
+            heisenberg.age = 6;
+            heisenberg.happiness = 0.8f;
+            heisenberg.cuteness = 12.5f;
+            heisenberg.baseAnimal();
+            heisenberg.Meow();
+
+
+            Console.ReadKey();
+        }
+    }
+}
